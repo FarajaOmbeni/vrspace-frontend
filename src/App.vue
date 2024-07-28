991e57e8974f3d69969d8d4fa3c1a76dc9a16968
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2024-07-28 07:01:42 +0300

    fixed syntax error in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Layout from '@/Layouts/Layout.vue';
 import { RouterView } from 'vue-router';
-import emailjs from 'emailjs/browser';
+import emailjs from '@emailjs/browser';
 
 emailjs.init("3QGzAfI8_ZPib_fRn");
 </script>
