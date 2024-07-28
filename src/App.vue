bb60644e9ec29e5d7e868c3d48deaecb597c34af
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2024-07-28 06:59:39 +0300

    initialized emailjs in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Layout from '@/Layouts/Layout.vue';
 import { RouterView } from 'vue-router';
+import emailjs from 'emailjs/browser';
+
+emailjs.init("3QGzAfI8_ZPib_fRn");
 </script>
 
 <template>
