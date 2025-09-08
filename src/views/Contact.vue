df6ee605bccf9c0e02f7e09287cc07f367449aa0
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-09-08 21:01:49 +0300

    updated the phone numbers

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -51,7 +51,9 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-900 mt-1 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                     </svg>
-                                    <p class="text-gray-900 text-lg font-bold">+254 729 054 606</p>
+                                    <p class="text-gray-900 text-lg font-bold">+254 729 054 607</p>
+                                    <p class="text-gray-900 text-lg font-bold">+254 793 643 308</p>
+                                    <p class="text-gray-900 text-lg font-bold">+254 100 366 713</p>
                                 </div>
                                 <div class="flex items-start gap-3">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-900 mt-1 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
